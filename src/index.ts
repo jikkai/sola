022fe22816bfbe4c89b553d40e7b97d5a1fd2816
Jikkai Xiao <sonne@asaki.me>
2017-02-18 02:28:54 +0800

    update cover

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './styles/main.css'
+
+const scrollDownEl = document.getElementById('scroll-down')
+if (scrollDownEl) {
+  scrollDownEl.onclick = () => {
+    const height = document.body.clientHeight
+    document.body.scrollTop = height
+  }
+}
