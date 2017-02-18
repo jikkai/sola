45708998a2eb61596acceb0feb2cbbab5ca923ad
Jikkai Xiao <sonne@asaki.me>
2017-02-18 14:10:59 +0800

    add smoothscroll

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import * as smoothscroll from 'smoothscroll-polyfill'
 import './styles/main.css'
+
+smoothscroll.polyfill()
 
 const scrollDownEl = document.getElementById('scroll-down')
 if (scrollDownEl) {
   scrollDownEl.onclick = () => {
-    const height = document.body.clientHeight
-    document.body.scrollTop = height
+    const height = document.body.clientHeight - document.body.scrollTop
+
+    window.scrollBy({ top: height, left: 0, behavior: 'smooth' })
   }
 }
