ba2c4b05e408db0f9b43d0bec4a75d48972ca826
Jikkai Xiao <sonne@asaki.me>
2017-02-20 18:56:27 +0800

    v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as smoothscroll from 'smoothscroll-polyfill'
+import * as hljs from 'highlightjs'
 import './styles/main.css'
 
+// smoothscroll
 smoothscroll.polyfill()
 
 const scrollDownEl = document.getElementById('scroll-down')
@@ -11,3 +13,6 @@ if (scrollDownEl) {
     window.scrollBy({ top: height, left: 0, behavior: 'smooth' })
   }
 }
+
+// highlight
+hljs.initHighlightingOnLoad()
